7e62bf07daf1a45e40c1e92348d42d6821b411ef
kr7348202 <kr7348202@gmail.com>
2026-03-06 18:33:59 +0530

    RoboShop

---
+++ mongodb.sh
@@ -0,0 +1,5 @@
+dnf install mongodb-org -y
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+# Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+systemctl enable mongod
+systemctl restart mongod
---
+++ redis.sh
@@ -0,0 +1,8 @@
+# THis remi repos is not working any more, So ignroe this step and move to next step directly. Run the commands from next step.
+# dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module disable redis -y
+dnf module enable redis:6 -y
+dnf install redis -y
+# Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
+systemctl enable redis
+systemctl restart redis

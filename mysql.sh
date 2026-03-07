a30051325e31fe4ed1529ae1d13b27ddba7a670c
kr7348202 <kr7348202@gmail.com>
2026-03-07 12:00:59 +0530

    RoboShop Project & Added DNS service

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,6 @@
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf module disable mysql -y
 dnf install mysql-community-server -y
-mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
 systemctl enable mysqld
-systemctl restart mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass RoboShop@1

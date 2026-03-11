a3d4bc315b2c802b34315009db4268e6c6fc08e4
chimtamreddy <7348202@gmail.com>
2026-03-11 21:40:02 +0530

    Roboshop Project & Added echo print

--- catalogue.sh
+++ catalogue.sh
@@ -1,34 +1,34 @@
 
-
+log=/tmp/roboshop.log
 echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Catalogue Service >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Mongo Repo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Disable NodeJS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module disable nodejs -y   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Enable NodeJS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Install NodeJS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+dnf install nodejs -y   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application User >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+useradd roboshop   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Remove Existing Directory>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
+rm -rf /app   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application Directory >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-mkdir /app &>>/tmp/roboshop.log
+mkdir /app   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Catalogue file >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Extract Catalogue Service >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip   &>>${log}
 cd /app
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Install NPM >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-npm install &>>/tmp/roboshop.log
+npm install   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Install Mongo Client >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+dnf install mongodb-org-shell -y   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Load Schema>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-mongo --host mongodb.anysite.info </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb.anysite.info </app/schema/catalogue.js   &>>${log}
 echo -e "\e[32m >>>>>>>>>>>>>>>>>>>>>>>>>>>>Start Catalogue Service>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl daemon-reload   &>>${log}
+systemctl enable catalogue   &>>${log}
+systemctl restart catalogue   &>>${log}
